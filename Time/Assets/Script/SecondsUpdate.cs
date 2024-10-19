23b42ee955c3c02059d2b12f5b66e47b2d1d7973
Margarida-coelho <162127564+Margarida-coelho@users.noreply.github.com>
2024-10-19 19:03:05 +0100

    Time
    Contents of time chapter

---
+++ Time/Assets/Script/SecondsUpdate.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class SecondsUpdate : MonoBehaviour
+{
+    float timeStartOffset = 0;
+    bool goStartTime = false;
+    public float speed = 0.5f;
+
+    void Update()
+    {
+        if ( !goStartTime )
+        {
+            timeStartOffset = Time.realtimeSinceStartup;
+            goStartTime = true;
+        }
+        this.transform.position = new Vector3(this.transform.position.x,
+                                              this.transform.position.y,
+                                              (Time.realtimeSinceStartup - timeStartOffset) * speed);
+    }
+    
+}
